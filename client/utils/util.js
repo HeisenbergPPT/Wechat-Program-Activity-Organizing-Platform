0462c6d155f64ad42d0690b173ed7dfdc766b800
Tong Pang <32211002+HeisenbergPPT@users.noreply.github.com>
2020-10-16 12:35:56 +0800

    Create util.js

---
+++ client/utils/util.js
@@ -0,0 +1,25 @@
+const formatTime = date => {
+
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+
+/**
+ * @desc: 格式化数字
+ * @return: n > 10 [eg: 12] => 12 | n < 10 [eg: 3] => '03'
+ * @param {*} n
+ */
+const formatNumber = n => {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
+
+module.exports = {
+  formatTime: formatTime
+}
