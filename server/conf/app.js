fcc5b9a7bdab3e0daffc8a883266b4c8b568ea60
Tong Pang <32211002+HeisenbergPPT@users.noreply.github.com>
2020-11-09 14:52:56 +0800

    Add files via upload

---
+++ server/conf/app.js
@@ -0,0 +1,9 @@
+module.exports = {
+    appConfig: {
+        appid: 'wxf1a6bd0b5585ddf0',
+        secret: '07dff28c99ed0f0b7ef8fd3862fc01fd'
+    },
+    userConfig: {
+        credit: 30          // 用户初始积分余额
+    }
+}
